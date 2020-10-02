1c34695658f3414c2ae2d224810ed2da19b8adab
Vahid Daneshmand <v.daneshmand@gmail.com>
2020-10-01 22:17:10 -0400

    Switch to CURL_SSLVERSION_TLSv1_1

--- R/GrapleR.R
+++ R/GrapleR.R
@@ -681,8 +681,8 @@ setMethod(f="GrapleCheckService",
           definition=function(grapleObject)
           {
             qurl<-paste(grapleObject@GWSURL, "service_status", sep="/")
-            CURL_SSLVERSION_TLSv1_2 <- 6L
-            opt <- RCurl::curlOptions(verbose = TRUE, sslversion = CURL_SSLVERSION_TLSv1_2)
+            CURL_SSLVERSION_TLSv1_1 <- 5L
+            opt <- RCurl::curlOptions(verbose = TRUE, sslversion = CURL_SSLVERSION_TLSv1_1)
             status<- getURL(qurl, .opts = opt)
             grapleObject@StatusCode <- 1
             grapleObject@StatusMsg <- paste(toString(fromJSON(status)[1]),toString(fromJSON(status)[2]))
